c580ab0b540f75067af8896aa9ff22b787a1b251
zqzal <936952020@qq.com>
2019-08-09 10:45:41 +0800

    xml add

--- app/src/main/java/com/xuxi/contactstest/MainActivity.java
+++ app/src/main/java/com/xuxi/contactstest/MainActivity.java
@@ -1,14 +1,51 @@
 package com.xuxi.contactstest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> adapter;
+
+    List<String> contactsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView contactsView = findViewById(R.id.contacts_view);
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,contactsList);
+
+        contactsView.setAdapter(adapter);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_CONTACTS},1);
+        }else {
+            readContacts();
+        }
+    }
+
+    private void readContacts() {
+
+        Cursor cursor = null;
+        //查询联系人数据
+        cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null ,null);
+
+
+
     }
 }
